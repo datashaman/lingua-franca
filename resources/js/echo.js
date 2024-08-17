b5b2e033a5b99ccedee0fd613226d399dbc1f084
Marlin Forbes <marlinf@datashaman.com>
2024-08-17 16:56:48 +0200

    try this

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,8 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['wss'],
+    enabledTransports: ['ws', 'wss'],
 });
